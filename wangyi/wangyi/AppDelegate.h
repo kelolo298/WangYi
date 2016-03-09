60b96beba60f04a8d135d27c2896125a527a25f3
kelolo <kelolo298@sina.com>
2016-03-09 13:44:21 +0800

    初始化

---
+++ wangyi/wangyi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wangyi
+//
+//  Created by hh on 16/3/9.
+//  Copyright © 2016年 JunCao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
